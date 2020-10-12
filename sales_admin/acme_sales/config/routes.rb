be7d8ae78907b4a2c5616d71c06420882cb1a960
Clare Glinka <clare@newknowledge.com>
2020-10-12 01:53:47 -0500

    basic views, etc

--- sales_admin/acme_sales/config/routes.rb
+++ sales_admin/acme_sales/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :sales
+  resources :products
+  resources :customers
+  resources :merchants
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sales#index'
+
+  post '/upload_sales', to: 'sales#upload_sales'
 end
