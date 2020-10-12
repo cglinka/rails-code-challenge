e413dacf022b9a59dde4040a69b312446c0b6dbf
Clare Glinka <clare@newknowledge.com>
2020-10-12 18:46:53 -0500

    Add auth

--- sales_admin/acme_sales/config/routes.rb
+++ sales_admin/acme_sales/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root 'sales#index'
+  post '/upload_sales', to: 'sales#upload_sales'
+
   resources :sales
   resources :products
   resources :customers
   resources :merchants
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'sales#index'
-
-  post '/upload_sales', to: 'sales#upload_sales'
 end
--- sales_admin/acme_sales/db/seeds.rb
+++ sales_admin/acme_sales/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {id: 1, password: "password", username: "default@acme.com"}
+])
